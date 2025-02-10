82ec26c4d3199475092ba734ce1d4cd2439a091c
juanc <juancruzliberati@hotmail.com>
2025-02-10 01:50:53 -0300

    Initial commit

---
+++ database/migrations/2025_02_10_044618_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('extract');
+            $table->longText('body');
+            $table->enum('status', [Category::BORRADOR, Category::PUBLICADO])->default(Category::BORRADOR);
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
